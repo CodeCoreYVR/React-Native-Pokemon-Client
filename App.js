61eb810b415fad9a1dcf5a39f8c9755c73d0dd85
Brandon Lam <brandonywlam@gmail.com>
2020-02-18 21:30:32 -0800

    💻 refactor header component

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native'; // React Native API Reference -> https://facebook.github.io/react-native/docs/activityindicator
+import { Header } from './components'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.header}><Text style={styles.h1}>Pokédex</Text></View>
+      <Header/>
     </View>
   );
 }
@@ -17,22 +18,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flexDirection: 'row'
   },
-  header: {
-    flex: 1,
-    backgroundColor: '#ee1515',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 20,
-    height: '20%',
-    borderBottomColor: '#222224',
-    borderBottomWidth: 5
-  },
-  h1: {
-    fontSize: 40,
-    color: '#FFCB05',
-    textShadowColor: 'black',
-    textShadowRadius: 10,
-    shadowOffset: {width: 2, height: 2},
-    shadowOpacity: 1,
-  }
 });
