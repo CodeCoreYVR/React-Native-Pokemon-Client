2282c58ef0545e62e9120832a207ef910eb1a078
Brandon Lam <brandonywlam@gmail.com>
2020-02-18 21:22:29 -0800

    💻add header

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'; // React Native API Reference -> https://facebook.github.io/react-native/docs/activityindicator
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}><Text style={styles.h1}>Pokédex</Text></View>
     </View>
   );
 }
@@ -12,8 +12,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f0f0',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    flexDirection: 'row'
+  },
+  header: {
+    flex: 1,
+    backgroundColor: '#ee1515',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 20,
+    height: '20%',
+    borderBottomColor: '#222224',
+    borderBottomWidth: 5
   },
+  h1: {
+    fontSize: 40,
+    color: '#FFCB05',
+    textShadowColor: 'black',
+    textShadowRadius: 10,
+    shadowOffset: {width: 2, height: 2},
+    shadowOpacity: 1,
+  }
 });
